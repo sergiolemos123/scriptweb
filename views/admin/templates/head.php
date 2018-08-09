a27ba6b96b198ea0590eb463974057ca7374f741
james1011 <solemos@misena.edu.co>
2018-08-08 20:23:46 -0500

    primer commit

---
+++ views/admin/templates/head.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+
+  <title></title>
+</head>
+<body>
+<nav>
+<ul class="topnav">
+  <li><a class="li1" href="#">Inicio</a></li>
+  <li><a href="#" class="li2" onclick="alert('Página En Construcción');">Integrantes</a></li>
+  <li><a href="#" class="li3" onclick="alert('Página En Construcción');">Proyectos</a></li>
+  <li class="li4"><a href="#">Cerrar Sesión</a></li>
+</ul>
+</nav>
